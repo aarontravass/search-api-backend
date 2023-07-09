93660c6d599f5c1acc2a88273eb09bbc76159693
aarontravass <aaron.travass@hotmail.com>
2023-07-09 00:45:50 -0400

    fix: fixed cors

--- server.ts
+++ server.ts
@@ -25,7 +25,11 @@ const csrfProtection = csurf({ cookie: true })
 const app = express()
 app.use(
   cors({
-    origin: ['localhost:4200', 'https://backend-sttjypqnpa-uc.a.run.app']
+    origin: ['localhost:4200', 'https://backend-sttjypqnpa-uc.a.run.app'],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200
   })
 )
 app.use(cookieParser())
